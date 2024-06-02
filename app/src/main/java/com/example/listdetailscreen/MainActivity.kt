60d5c578952405b004afa1a6ed75187f4a756f7f
navneet <navneetpalgithub@gmail.com>
2024-06-02 11:03:56 +0530

    Marssy

--- app/src/main/java/com/example/listdetailscreen/MainActivity.kt
+++ app/src/main/java/com/example/listdetailscreen/MainActivity.kt
@@ -6,6 +6,25 @@ import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.adaptive.ExperimentalMaterial3AdaptiveApi
+import androidx.compose.material3.adaptive.layout.ListDetailPaneScaffoldRole
+import androidx.compose.material3.adaptive.navigation.NavigableListDetailPaneScaffold
+import androidx.compose.material3.adaptive.navigation.rememberListDetailPaneScaffoldNavigator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.listdetailscreen.ui.theme.ListDetailScreenTheme
 
 
@@ -21,9 +40,62 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ListDetailScreenTheme {
+                Scaffold { paddingValues ->
 
+
+                }
             }
         }
     }
+
+}
+
+@OptIn(ExperimentalMaterial3AdaptiveApi::class)
+@Composable
+fun ListDetailLayout(
+    modifier: Modifier = Modifier,
+) {
+
+    val navigator = rememberListDetailPaneScaffoldNavigator<Any>()
+
+    NavigableListDetailPaneScaffold(
+        navigator = navigator,
+        listPane = {
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(),
+                contentPadding = PaddingValues(16.dp)
+            ) {
+                items(100) {
+                    Text(
+                        text = "Item $it",
+                        modifier = Modifier
+                            .fillParentMaxWidth()
+                            .clickable {
+                                navigator.navigateTo(
+                                    pane = ListDetailPaneScaffoldRole.Detail,
+                                    content = "Item $it"
+                                )
+                            }
+                            .padding(16.dp)
+                    )
+                }
+            }
+        },
+        detailPane = {
+            val content = navigator.currentDestination?.content?.toString() ?: "Select an Item"
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(MaterialTheme.colorScheme.primaryContainer),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ){
+                Text(text = content)
+            }
+        }
+    )
+
 
 }
