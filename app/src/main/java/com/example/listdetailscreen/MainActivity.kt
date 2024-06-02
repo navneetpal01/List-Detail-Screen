74df88c26204af1203719ed53c83fcfc15a20d0c
navneet <navneetpalgithub@gmail.com>
2024-06-02 11:12:59 +0530

    Marssy

--- app/src/main/java/com/example/listdetailscreen/MainActivity.kt
+++ app/src/main/java/com/example/listdetailscreen/MainActivity.kt
@@ -9,11 +9,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.AssistChip
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -41,8 +46,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             ListDetailScreenTheme {
                 Scaffold { paddingValues ->
-
-
+                    ListDetailLayout(
+                        modifier = Modifier.padding(paddingValues)
+                    )
                 }
             }
         }
@@ -59,6 +65,7 @@ fun ListDetailLayout(
     val navigator = rememberListDetailPaneScaffoldNavigator<Any>()
 
     NavigableListDetailPaneScaffold(
+        modifier = modifier,
         navigator = navigator,
         listPane = {
             LazyColumn(
@@ -91,6 +98,41 @@ fun ListDetailLayout(
                     .background(MaterialTheme.colorScheme.primaryContainer),
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(text = content)
+                Row {
+                    AssistChip(
+                        onClick = {
+                                  navigator.navigateTo(
+                                      pane = ListDetailPaneScaffoldRole.Extra,
+                                      content = "Option 1"
+                                  )
+                        },
+                        label = {
+                            Text(text = "Option 1")
+                        }
+                    )
+                    Spacer(modifier = Modifier.width(16.dp))
+                    AssistChip(
+                        onClick = {
+                            navigator.navigateTo(
+                                pane = ListDetailPaneScaffoldRole.Extra,
+                                content = "Option 2"
+                            )
+                        },
+                        label = {
+                            Text(text = "Option 2")
+                        }
+                    )
+                }
+            }
+        },
+        extraPane = {
+            val content = navigator.currentDestination?.content?.toString() ?: "Select an Option"
+            Box(
+                modifier = Modifier
+                    .fillMaxSize(),
+                contentAlignment = Alignment.Center
             ){
                 Text(text = content)
             }
